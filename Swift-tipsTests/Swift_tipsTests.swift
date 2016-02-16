8857ec606372eb34e932305dbb80c7b323c42ea6
長谷川瞬哉 <s-hasegawa@infocity.co.jp>
2016-02-16 17:27:22 +0900

    Initial Commit

---
+++ Swift-tipsTests/Swift_tipsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_tipsTests.swift
+//  Swift-tipsTests
+//
+//  Created by 長谷川瞬哉 on 2016/02/16.
+//  Copyright © 2016年 長谷川瞬哉. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_tips
+
+class Swift_tipsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
